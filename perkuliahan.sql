445c11d99805fe588f66e18e7ea4be8fd9195c36
Divaaswinsyach <71751665+Divaaswinsyach@users.noreply.github.com>
2020-11-20 20:48:15 +0700

    Add files via upload

---
+++ perkuliahan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2020 at 02:45 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perkuliahan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(5) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nim` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`) VALUES
+(1, 'Almaghfirah Khalifah Diva A.', 19510043),
+(2, 'Siti', 19510050),
+(5, 'Susan', 19510110);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tambah.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD</title>
+</head>
+<body style="background-color: skyblue">
+	<center>
+		<h2><font color="purple">DATA MAHASISWA PRODI SISTEM INFORMASI</font></h2>
+		<br>
+		<h3>TAMBAH DATA</h3>
+		<form method="post" action="tambah-proses.php">
+			<table>
+				<tr>
+					<td>Nama</td>
+					<td><input type="text" name="nama"></td>
+				</tr>
+				<tr>
+					<td>NIM</td>
+					<td><input type="number" name="nim"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="SIMPAN"></td>
+				</tr>
+			</table>
+		</form>
+	</center>
+</body>
+</html>
